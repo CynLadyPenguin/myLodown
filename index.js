e0abae371450f42a372ee046a2bbd27c93d44773
CynLadyPenguin <ladypenguin1991@gmail.com>
2022-10-11 20:58:34 +0000

    myLodown

--- index.js
+++ index.js
@@ -3,6 +3,18 @@
 // YOU KNOW WHAT TO DO //
 
 /**
+ * identity: This function is designed to take in a value and return that value
+ * unchanged.
+ * 
+ * @param {value} any: this value can be any type
+ * @return {value} any: the return value is the unchanged parameter value
+ */
+function identity(value){
+    return value;
+}
+module.exports.identity = identity;
+
+/**
  * each: Designed to loop over a collection, Array or Object, and applies the 
  * action Function to each value in the collection.
  * 
@@ -22,3 +34,34 @@ function each(collection, action) {
     }
 }
 module.exports.each = each;
+
+/**
+ * 
+ * typeOf: function is designed to return the type of the input value
+ * 
+ * @param {value} any: The input value is one of string, array, object, undefined,
+ * number, boolean, null, or function
+ * @return {string} : The return is a string representing the type of the input
+ * value
+ */
+function typeOf(value){
+    if(Array.isArray(value)){
+        return "array";
+    } else if(value === null){
+        return "null";
+    } else if(value === undefined){
+        return "undefined";
+    } else {
+        return typeof value;
+    }
+}
+module.exports.typeOf = typeOf;
+
+/**
+ * 
+ * first: This function is designed to take in an array and a number. If the 
+ * input value isn't an array, return an empty array. If there is no input number, 
+ * return the first element of the array. If the input number is greater than 
+ * the length of the array, return the whole array. If the number is negative, 
+ * return an empty array
+ */
